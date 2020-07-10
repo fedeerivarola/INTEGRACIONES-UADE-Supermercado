bf02eb6d23adaea675bdc9067b5ce9e8d6c5e09f
Federico A. Rivarola <federicorivarola@outlook.com>
2020-07-10 03:33:25 -0300

    abm empleados

--- src/components/EmpleadoABM/EmpleadoABM.js
+++ src/components/EmpleadoABM/EmpleadoABM.js
@@ -2,53 +2,92 @@ import React, { useState } from 'react'
 import './EmpleadoABM.css'
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { Modal, Fade, Backdrop, List, ListItem, ListItemText, ListItemAvatar, Avatar, ListItemSecondaryAction } from '@material-ui/core';
+import { Button, Modal, Fade, Backdrop, List, ListItem, ListItemText, ListItemAvatar, Avatar, ListItemSecondaryAction } from '@material-ui/core';
 
 
 const EmpleadoABM = (props) => {
 
-    //este array va a venir por props
-    let emptest = [
-        {
-            legajo: '001',
-            nombre: 'Pepe',
-            sector: 'Ventas'
-        }, {
-            legajo: '002',
-            nombre: 'Marco',
-            sector: 'Encargado'
-        }, {
-            legajo: '003',
-            nombre: 'Pepe2',
-            sector: 'Ventas'
-        }, {
-            legajo: '004',
-            nombre: 'Pepe3',
-            sector: 'RRHH'
-        }, {
-            legajo: '005',
-            nombre: 'Alberto',
-            sector: 'Sistemas'
-        }, {
-            legajo: '006',
-            nombre: 'Fernando',
-            sector: 'Sistemas'
-        }, {
-            legajo: '007',
-            nombre: 'Milei',
-            sector: 'Finanzas'
-        }]
-
-    const admin = props.admin;
-    const [empleados, setEmpleados] = useState(emptest);
+    //const admin = props.admin;
+    const [empleados, setEmpleados] = useState(props.empleados);
     const [openModal, setOpenModal] = useState(false);
     const [dense, setDense] = React.useState(false);
-    const [secondary, setSecondary] = React.useState(false);
+    const [selectedEmp, setSelectedEmp] = useState(null);
+    const [newEmpleado, setNewEmpleado] = useState({ legajo: '', nombre: '', sector: '' });
+
+
+    function createEmpleado() {
+        let updEmpleados = empleados
+        updEmpleados.push(newEmpleado)
+        setEmpleados(updEmpleados)
+        setOpenModal(false)
+    }
+
+    function deleteEmpleado(e) {
+
+        let updEmpleados = empleados
+
+        let i = updEmpleados.indexOf(e);
+        i !== -1 && updEmpleados.splice(i, 1);
+
+        setEmpleados(updEmpleados);
+
+    }
+
+    function handleInput(e) {
+
+        let newEmp = newEmpleado
+
+        if (e.target.id === 'legajo') {
+            newEmp.legajo = e.target.value;
+        } else if (e.target.id === 'nombre') {
+            newEmp.nombre = e.target.value;
+        } else if (e.target.id === 'sector') {
+            newEmp.sector = e.target.value;
+        }
+
+        setNewEmpleado(newEmp);
+    }
+
+    function formCreateEmpleado() {
+        return (
+            <div className="formNewEmp">
+                <h2>Registrar nuevo empleado</h2>
+                <p>Legajo</p>
+                <input id='legajo' onChange={(e) => handleInput(e)} />
+                <p>Nombre</p>
+                <input id='nombre' onChange={(e) => handleInput(e)} />
+                <p>Sector</p>
+                <input id='sector' style={{ marginBottom: "20px" }} onChange={(e) => handleInput(e)} />
+
+                <Button variant="contained" color="primary" onClick={() => createEmpleado()}>GUARDAR</Button>
+            </div>
+        )
+    }
+
+    function viewProfile() {
+
+        return (
+            <div>
+                <h2>View profile empleado</h2>
+                <h4>Legajo: </h4>
+                <p>{selectedEmp.legajo}</p>
+                <h4>Nombre: </h4>
+                <p>{selectedEmp.nombre}</p>
+                <h4>Sector: </h4>
+                <p>{selectedEmp.sector}</p>
+            </div>
+        )
+    }
 
     function renderList(e) {
 
         return (
-            <ListItem key={e.legajo} button onClick={() => setOpenModal(true)}>
+            <ListItem key={e.legajo} button
+                onClick={() => {
+                    console.log(e)
+                    setOpenModal(true);
+                    setSelectedEmp(e);
+                }}>
                 <ListItemAvatar className="w-24">
                     <Avatar />
                 </ListItemAvatar>
@@ -65,30 +104,23 @@ const EmpleadoABM = (props) => {
         )
     }
 
-    function deleteEmpleado(e) {
-
-        console.log(e)
-
-        let updEmpleados = empleados
-
-        let i = updEmpleados.indexOf(e);
-        i !== -1 && updEmpleados.splice(i, 1);
-
-        setEmpleados(updEmpleados);
-
-    }
-
     return (
         <div className="menu-empleados">
-            <div className="botonera-empleados">
-                <button>Agregar Empleado</button>
-                <button>Borrar Empleado</button>
-                <button>Modificar Empleado</button>
-            </div>
+
+            <h2>Lista de Empleados</h2>
             <div className="list-empleados">
                 <List dense={dense}>
                     {empleados.map((e) => renderList(e))}
                 </List>
+            </div>
+
+            <div className="botonera-empleados">
+                <Button variant="contained" color="primary"
+                    onClick={() => {
+                        setSelectedEmp(null)
+                        setOpenModal(true)
+                    }}
+                >Agregar Empleado</Button>
             </div>
 
             <Modal className="modal"
@@ -104,11 +136,11 @@ const EmpleadoABM = (props) => {
             >
                 <Fade in={openModal}>
                     <div className="profile-empleado">
-                        <p>View profile empleado</p>
+                        {selectedEmp ? viewProfile() : formCreateEmpleado()}
                     </div>
                 </Fade>
             </Modal>
-        </div>
+        </div >
     )
 
 }
